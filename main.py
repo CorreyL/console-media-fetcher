150d020932ca29c592703e138479df92538ee4b4
Correy Lim <correylim@gmail.com>
2020-03-21 12:05:15 -0700

    Add logic to parse relevant contents of each tweet
    - Use the `API.home_timeline` function to retrieve all the tweets of the
      Twitter account specified through the API tokens provided
    - Iterate through each tweet and obtain:
      * The URL to the media image file
      * The date and time of when the tweet was created
      * Formatting the date and time to use as a filename, ensuring the
        filename is compatible with Windows machines (i.e. no `:`
        characters)
      * The file type of the image file (i.e. `.jpg`, `.png`, etc.)
      Put all the above information in a `list` to be parsed through in
      subsequent logic
    - Wrap the logic in a `main()` function to make unit testing easier in
      the future

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import tweepy
 import yaml
+from pytz import timezone
 
 with open("tokens.yaml") as tokens:
     tokens = yaml.load(tokens, Loader=yaml.FullLoader)
@@ -8,3 +9,33 @@ auth = tweepy.OAuthHandler(tokens["api_key"], tokens["api_secret_key"])
 auth.set_access_token(tokens["access_token"], tokens["access_token_secret"])
 
 api = tweepy.API(auth)
+
+public_tweets = api.home_timeline()
+
+
+def main():
+    media_files = []
+
+    for tweet in public_tweets:
+        date = tweet.created_at.astimezone()
+        # Replace `:` characters with `.` to ensure the file name is usable on
+        # Windows machines
+        # Replace ` ` characters with `_` to make filename usage easier without
+        # needing to escape the ` ` characters if ever used in a script
+        reformatted_date = str(date).replace(":", ".").replace(" ", "_")
+        media = tweet.entities.get("media", [])
+        if len(media) > 0:
+            media_url = media[0]["media_url"]
+            file_type = str(media_url).split(".")[-1]
+            media_files.append(
+                {
+                    "media_url": media_url,
+                    "datetime_tweeted": date,
+                    "file_name": reformatted_date,
+                    "file_type": file_type,
+                }
+            )
+
+
+if __name__ == "main":
+    main()
