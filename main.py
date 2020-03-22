b24af9415e9ab83e7dead37e5b938593e38a1a29
Correy Lim <correylim@gmail.com>
2020-03-22 11:24:48 -0700

    Add support for batches of media
    Tweets can contain more than one photo, but previous logic did not take
    this into account, resulting in only one of the photos being downloaded
    
    Rather than interrogating the `tweet.entities`, in which
    `tweet.entities.media` will only contain one of the possible four
    photos, immediately interrogate `tweet.extended_entities`, and iterate
    through the `media` list rather than just obtaining the first element of
    the `media` list
    
    Since `extended_entities` is being interrogated immediately, there is no
    need to retrieve the `media` list explicitly from `extended_entities` in
    the `video` retrieval logic
    
    If more than one photo exists in a tweet, then they would all have the
    same file name in the current logic. As such, call `enumerate` on
    `all_media` and give each file a `batch_number`, ensuring that each file
    is given a unique file name when processing a tweet with more than one
    photo

--- main.py
+++ main.py
@@ -43,38 +43,38 @@ def main():
         # Replace ` ` characters with `_` to make filename usage easier without
         # needing to escape the ` ` characters if ever used in a script
         reformatted_date = str(date).replace(":", ".").replace(" ", "_")
-        media = tweet.entities.get("media", [])
-        if len(media) > 0:
-            media_url = media[0]["media_url"]
-            file_type = str(media_url).split(".")[-1]
-            # The media_url will have `video` as part of the path if the file is
-            # a video
-            if "video" in media_url:
-                # Traverse the structure of the `extended_entities` object of
-                # the tweet to get the different processed video sizes
-                processed_videos = (
-                    tweet.extended_entities.get("media")[0]
-                    .get("video_info")
-                    .get("variants")
+        all_media = tweet.extended_entities.get("media", [])
+        if len(all_media) > 0:
+            for idx, media in enumerate(all_media):
+                media_url = media["media_url"]
+                file_type = str(media_url).split(".")[-1]
+                # The media_url will have `video` as part of the path if the file is
+                # a video
+                if "video" in media_url:
+                    # Traverse the structure of the `extended_entities` object of
+                    # the tweet to get the different processed video sizes
+                    processed_videos = media.get("video_info").get("variants")
+                    sorted_videos = sorted(
+                        processed_videos, key=lambda obj: obj.get("bitrate", 0),
+                    )
+                    # TODO Make this a script argument that can be changed so that
+                    # the different video qualities can be selected
+                    highest_bitrate_video = sorted_videos[-1]
+                    media_url = highest_bitrate_video["url"]
+                    # Parse the string such as `video/mp4`
+                    file_type = highest_bitrate_video["content_type"].split(
+                        "/"
+                    )[-1]
+                media_files.append(
+                    {
+                        "media_url": media_url,
+                        "datetime_tweeted": date,
+                        "file_name": reformatted_date,
+                        "file_type": file_type,
+                        "platform": platform,
+                        "batch_number": idx + 1,
+                    }
                 )
-                sorted_videos = sorted(
-                    processed_videos, key=lambda obj: obj.get("bitrate", 0),
-                )
-                # TODO Make this a script argument that can be changed so that
-                # the different video qualities can be selected
-                highest_bitrate_video = sorted_videos[-1]
-                media_url = highest_bitrate_video["url"]
-                # Parse the string such as `video/mp4`
-                file_type = highest_bitrate_video["content_type"].split("/")[-1]
-            media_files.append(
-                {
-                    "media_url": media_url,
-                    "datetime_tweeted": date,
-                    "file_name": reformatted_date,
-                    "file_type": file_type,
-                    "platform": platform,
-                }
-            )
 
     if not os.path.exists(destination_folder_name):
         os.mkdir(destination_folder_name)
@@ -83,12 +83,14 @@ def main():
         media_url = obj["media_url"]
         file_name = obj["file_name"]
         file_type = obj["file_type"]
+        batch_number = obj["batch_number"]
         platform = obj["platform"]
         destination = f"./{destination_folder_name}/{platform}"
         if not os.path.exists(destination):
             os.mkdir(destination)
         download = (
-            f"./{destination_folder_name}/{platform}/{file_name}.{file_type}"
+            f"./{destination_folder_name}/{platform}/"
+            f"{file_name}_{batch_number}.{file_type}"
         )
         if not os.path.exists(download):
             urllib.request.urlretrieve(
